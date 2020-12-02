a043911bafa0e7980087e12be5f1e8802d055f31
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2020-12-02 22:44:05 +0700

    Init project

---
+++ WeSplitApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeSplitApp.View;
+
+namespace WeSplitApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    if (MainGrid == null)
+                    {
+                        return;
+                    }
+                    //HomeScreen
+                    MainGrid.Children.Clear();
+                    MainGrid.Children.Add(new HomeScreen());
+                    break;
+                case 1:
+                    MainGrid.Children.Clear();
+                    //SearchScreen
+                    //MainGrid.Children.Add(new DetailScreen());
+                    break;
+                case 2:
+                    MainGrid.Children.Clear();
+                    //AddScreen
+                    MainGrid.Children.Add(new AddScreen());
+                    break;
+            }
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
